147880315a63ef26fdc99efd18680b1d91e8bbab
Guch1120 <e1x22089@oit.ac.jp>
2025-06-07 03:16:05 +0900

    termnatorの起動までを確認

---
+++ docker/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# ROS2 Humbleの環境設定を読み込む
+source /opt/ros/humble/setup.bash
+
+# terminatorの設定ファイルがあればコピーする
+if [ -f "/home/dockeruser/terminator_config/config" ]; then
+  cp /home/dockeruser/terminator_config/config /home/dockeruser/.config/terminator/config
+  chown dockeruser:dockeruser /home/dockeruser/.config/terminator/config
+fi
+
+# もし既に動いてたら起動しない
+if ! pgrep -x "terminator" > /dev/null; then
+  # terminatorをバックグラウンドで起動
+  dbus-launch terminator &
+else
+  echo "terminator is already running."
+fi
+
+
+# 渡されたコマンドを実行する
+exec "$@"
