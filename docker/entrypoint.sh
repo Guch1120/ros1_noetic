904072ac8e153d81efdb1a554cfb837f6713e562
Guch1120 <e1x22089@oit.ac.jp>
2025-06-13 03:07:27 +0900

    ui崩れてるけど表示できた

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 set -e
 
-echo "yakiniku" | sudo -S service dbus start 2>/dev/null
-
-# ROS2 Humbleの環境設定を読み込む
+# ROS環境を読み込む
 source /opt/ros/humble/setup.bash
-
-if [ -f "/opt/ros2_ws/install/setup.bash" ]; then
-  source /opt/ros2_ws/install/setup.bash
+if [ -f "/home/dockeruser/ros2_ws/install/setup.bash" ]; then
+  source /home/dockeruser/ros2_ws/install/setup.bash
 fi
 
 # terminatorの設定ファイルがあればコピーする
 if [ -f "/home/dockeruser/terminator_config/config" ]; then
+  mkdir -p /home/dockeruser/.config/terminator
   cp /home/dockeruser/terminator_config/config /home/dockeruser/.config/terminator/config
   chown dockeruser:dockeruser /home/dockeruser/.config/terminator/config
 fi
 
-# もし既に動いてたら起動しない
-if ! pgrep -x "terminator" > /dev/null; then
-  # terminatorをバックグラウンドで起動
-  dbus-launch terminator &
-else
-  echo "terminator is already running."
-fi
-
-
-# 渡されたコマンドを実行する
-exec "$@"
+# dbus-run-session を使って、クリーンなD-Busセッションを開始し、
+# その中でterminatorと、コンテナのメインコマンドを実行する
+exec dbus-run-session -- bash -c '
+  set -e
+  terminator &
+  sleep 1
+  exec "$@"
+'
---
+++ scripts/setup_flexbe.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -e
+
+echo "--- FlexBEワークスペースのセットアップを開始します ---"
+
+WS_DIR=~/ros2_ws
+mkdir -p $WS_DIR/src
+cd $WS_DIR
+
+# --- 1. FlexBEのリポジトリを準備 ---
+echo "--- 1. Cloning FlexBE repositories ---"
+if [ ! -d "src/flexbe_behavior_engine" ]; then
+    git clone -b humble https://github.com/FlexBE/flexbe_behavior_engine.git src/flexbe_behavior_engine
+else
+    echo "flexbe_behavior_engine は既に存在します。"
+fi
+
+if [ ! -d "src/flexbe_app" ]; then
+    git clone -b humble https://github.com/FlexBE/flexbe_app.git src/flexbe_app
+else
+    echo "flexbe_app は既に存在します。"
+fi
+
+# --- 1.5. nwjs_install スクリプトを自動修正 ---
+echo "--- 1.5. Patching nwjs_install script ---"
+NWJS_INSTALL_SCRIPT=src/flexbe_app/bin/nwjs_install
+
+# curl に -L オプションを追加して、リダイレクトを追跡できるようにする
+sed -i 's/curl -s -o/curl -sL -o/' $NWJS_INSTALL_SCRIPT
+# VERSION変数を、実際にダウンロードするバージョンに合わせる
+sed -i 's/VERSION="v0.55.0"/VERSION="v0.83.0"/' $NWJS_INSTALL_SCRIPT
+echo "nwjs_install script has been patched."
+
+
+# --- 2. 依存関係をインストール ---
+echo "--- 2. Installing dependencies with rosdep ---"
+source /opt/ros/humble/setup.bash
+sudo apt-get update # NOTE: 2回目以降は不要な場合が多い
+rosdep update
+rosdep install --from-paths src --ignore-src -r -y
+
+
+# --- 3. ワークスペースをビルド ---
+echo "--- 3. Building the workspace with colcon ---"
+sudo chown -R dockeruser:dockeruser ~/ros2_ws
+colcon build --symlink-install
+
+# --- 4. nwjsをインストール (パッチ済み) ---
+echo "--- 4. Installing nwjs ---"
+source $WS_DIR/install/setup.bash
+ros2 run flexbe_app nwjs_install
+
+echo "--- セットアップが完了しました！ ---"
+echo "次のコマンドで環境設定を読み込んでください: source ~/ros2_ws/install/setup.bash"
